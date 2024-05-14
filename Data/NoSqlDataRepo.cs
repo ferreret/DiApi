26a97fb5e8c6b4391b202d5c5f740c2be870e25a
Nicolás Barceló <ferreret@gmail.com>
2024-05-14 13:59:45 +0200

    Finished simple sample DI

--- Data/NoSqlDataRepo.cs
+++ Data/NoSqlDataRepo.cs
@@ -1,8 +1,8 @@
 namespace DiApi.Data
 {
-    public class NoSqlDataRepo 
+    public class NoSqlDataRepo : IDataRepo
     {
-        public string GetData()
+        public string ReturnData()
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("--> Getting data from No SQL Database...");
--- Program.cs
+++ Program.cs
@@ -2,16 +2,16 @@ using DiApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// builder.Services.AddScoped<IDataRepo, NoSqlDataRepo>();
+builder.Services.AddScoped<IDataRepo, SqlDataRepo>();
+
 var app = builder.Build();
+
 app.UseHttpsRedirection();
 
-app.MapGet("/getdata", () =>
+app.MapGet("/getdata", (IDataRepo repo) =>
 {
-    // var repo = new SqlDataRepo();
-     var repo = new NoSqlDataRepo();    
-
-    // repo.ReturnData();
-    repo.GetData();
+    repo.ReturnData();
 
     return Results.Ok();
 });
